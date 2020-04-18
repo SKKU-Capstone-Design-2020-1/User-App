45bfb88d7ed88b65c7ccd34ba0fabc1c19a0d42a
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-04-18 20:36:59 +0900

    Add butterknife

--- app/src/main/java/com/skku/userweb/MainActivity.java
+++ app/src/main/java/com/skku/userweb/MainActivity.java
@@ -2,7 +2,7 @@ package com.skku.userweb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
