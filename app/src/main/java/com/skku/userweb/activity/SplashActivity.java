899343d2b944b76d3b88c3acd2d0b2b50ada26b2
Jennydream <1791tnfla@gmail.com>
2020-04-22 06:22:42 +0900

    Add splash, Login and Contact Activity

--- app/src/main/java/com/skku/userweb/activity/SplashActivity.java
+++ app/src/main/java/com/skku/userweb/activity/SplashActivity.java
@@ -3,8 +3,9 @@ package com.skku.userweb.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.os.Handler;
 import com.skku.userweb.R;
+import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +13,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+       /*Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
+
+    protected  void  onPause(){
+        super.onPause();
+        finish();
+    }*/
+    }
+
+
 }
