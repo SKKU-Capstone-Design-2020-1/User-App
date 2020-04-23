af50f1eba3a1192d85a30d36c533e2b78941714a
Minki-kim95 <rlaalsrl7777@gmail.com>
2020-04-24 05:47:57 +0900

    [ADD] make UI of UserFragment

--- app/src/main/java/com/skku/userweb/fragment/UserFragment.java
+++ app/src/main/java/com/skku/userweb/fragment/UserFragment.java
@@ -1,17 +1,21 @@
 package com.skku.userweb.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.skku.userweb.R;
+import com.skku.userweb.activity.EditUserActivity;
 
 
-public class UserFragment extends Fragment {
+public class UserFragment extends Fragment implements View.OnClickListener {
 
 
     @Override
@@ -23,6 +27,25 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_user, container, false);
+
+        Button editbutton = (Button) rootView.findViewById(R.id.fragment_user_editButton);
+        Button logoutbutton = (Button) rootView.findViewById(R.id.fragment_user_logoutButton);
+
+        editbutton.setOnClickListener(this);
+        logoutbutton.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fragment_user_editButton:
+                //go to edit user activity
+            case R.id.fragment_user_logoutButton:
+                //logout
+
+        }
     }
 }
