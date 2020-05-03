289950de9f178199ec3baf7634a0c727c8a55095
Jennydream <1791tnfla@gmail.com>
2020-05-04 01:04:33 +0900

    Add splash dark background, edit login and contact activity

--- app/src/main/java/com/skku/userweb/activity/SignUpActivity.java
+++ app/src/main/java/com/skku/userweb/activity/SignUpActivity.java
@@ -1,23 +1,26 @@
 package com.skku.userweb.activity;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.content.Intent;
 
 import com.skku.userweb.R;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class SignUpActivity extends AppCompatActivity  {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
-        Button button=(Button) findViewById(R.id.activity_login_Login);
+        Button button=(Button) findViewById(R.id.activity_Signup_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,12 +29,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-        TextView textView = (TextView) findViewById(R.id.activity_login_register);
+        TextView textView = (TextView) findViewById(R.id.activity_Signup_login);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(getApplicationContext(), SignUpActivity.class); //register activity로 바꿔야 함
+                Intent intent= new Intent(getApplicationContext(), LoginActivity.class); //register activity로 바꿔야 함
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/skku/userweb/util/Constants.java
+++ app/src/main/java/com/skku/userweb/util/Constants.java
@@ -4,10 +4,11 @@ import com.skku.userweb.activity.EditUserActivity;
 import com.skku.userweb.activity.LoginActivity;
 import com.skku.userweb.activity.MainActivity;
 import com.skku.userweb.activity.SelectStoreActivity;
+import com.skku.userweb.activity.SignUpActivity;
 import com.skku.userweb.activity.SplashActivity;
 
 public class Constants {
     public static final Class[] activities = {
-            MainActivity.class, SplashActivity.class, LoginActivity.class, SelectStoreActivity.class, EditUserActivity.class
+            MainActivity.class, SplashActivity.class, LoginActivity.class, SelectStoreActivity.class, EditUserActivity.class, SignUpActivity.class
     };
 }
