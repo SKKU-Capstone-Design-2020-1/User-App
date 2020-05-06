76cf789c41b7d7cee356d15390267d6fe43c8d89
khsung <khsung0@nate.com>
2020-05-07 02:21:50 +0900

    Modify SelectStoreActivity
    ListView -> RecyclerView(+CardView)

--- app/src/main/java/com/skku/userweb/activity/SelectStoreActivity.java
+++ app/src/main/java/com/skku/userweb/activity/SelectStoreActivity.java
@@ -1,48 +1,42 @@
 package com.skku.userweb.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+
 import android.widget.Toast;
 
 import com.skku.userweb.R;
 import com.skku.userweb.adapter.StoreListAdapter;
 import com.skku.userweb.util.Store;
 
-import java.util.ArrayList;
 
 public class SelectStoreActivity extends AppCompatActivity {
-
-    ArrayList<Store> storeList;
+    RecyclerView recyclerView;
+    StoreListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_store);
-        this.InitializeStore();
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new StoreListAdapter(getApplicationContext());
+        adapter.addItem(new Store(R.drawable.seoul_skku,"Seoul-sungkyunkwan-library","25-2 Sungkyunkwan-ro, Jongno-gu, Seoul","25/46"));
+        adapter.addItem(new Store(R.drawable.suwon_skku,"Suwon-sungkyunkwan-library","2066, Seobu-ro, Jangan-gu, Suwon-si, Gyeonggi-do","31/53"));
 
-        ListView listView = findViewById(R.id.listView);
-        final StoreListAdapter storeListAdapter = new StoreListAdapter(this,storeList);
+        recyclerView.setAdapter(adapter);
 
-        listView.setAdapter(storeListAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adapter.setOnItemClickListener(new StoreListAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                //String name=storeListAdapter.getItem(position).getStoreName();              //store name 변수로 저장해서 보낼준비
-                Toast.makeText(getApplicationContext(),storeListAdapter.getItem(position).getStoreName(),Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(SelectStoreActivity.this,MainActivity.class);
-                startActivity(intent);
+            public void onItemClick(RecyclerView.ViewHolder holder, View view, int position) {
+                Store item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),item.getStoreName(), Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private void InitializeStore() {
-        storeList = new ArrayList<Store>();
-        storeList.add(new Store(R.drawable.seoul_skku,"Seoul-sungkyunkwan-library","25-2 Sungkyunkwan-ro, Jongno-gu, Seoul","25/46"));
-        storeList.add(new Store(R.drawable.suwon_skku,"Suwon-sungkyunkwan-library","2066, Seobu-ro, Jangan-gu, Suwon-si, Gyeonggi-do","31/53"));
     }
 }
