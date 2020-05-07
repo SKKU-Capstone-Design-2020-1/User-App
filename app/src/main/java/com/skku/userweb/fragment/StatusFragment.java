6ee550a231dc58600b83db4d5cc65c9119088b5a
Minki-kim95 <rlaalsrl7777@gmail.com>
2020-05-07 15:20:45 +0900

    [ADD] step progress bar in Fragment status

--- app/src/main/java/com/skku/userweb/fragment/StatusFragment.java
+++ app/src/main/java/com/skku/userweb/fragment/StatusFragment.java
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.kofigyan.stateprogressbar.StateProgressBar;
 import com.skku.userweb.R;
 
 
 public class StatusFragment extends Fragment {
 
+    String[] descriptionData = {"approach", "Using", "timeout"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,32 @@ public class StatusFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_status, container, false);
+
+        StateProgressBar stateProgressBar = (StateProgressBar) rootView.findViewById(R.id.fragment_status_state_progress_bar);
+        stateProgressBar.setStateDescriptionData(descriptionData);
+
+        Button stepSwitchButton = (Button) rootView.findViewById(R.id.fragment_status_stepSwitch_button);
+
+        stepSwitchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (stateProgressBar.getCurrentStateNumber()){
+                    case 1:
+                        stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                        break;
+                    case 2:
+                        stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE);
+                        break;
+                    case 3:
+                        stateProgressBar.setAllStatesCompleted(true);
+                        break;
+                }
+
+            }
+        });
+
+        return rootView;
     }
 
 }
