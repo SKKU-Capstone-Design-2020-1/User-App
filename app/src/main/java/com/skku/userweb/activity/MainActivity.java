376e4ca4cd77ac8a0b52077d37ad2e10114a6a68
khsung <khsung0@nate.com>
2020-05-31 17:25:39 +0900

    Add image and Modify data output method

--- app/src/main/java/com/skku/userweb/activity/MainActivity.java
+++ app/src/main/java/com/skku/userweb/activity/MainActivity.java
@@ -6,11 +6,21 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -71,9 +81,33 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText("image" + position);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    Spanned imageText = Html.fromHtml("<img src=\"icon_drag\" width=\"50\" height=\"50\">", new Html.ImageGetter() {
+                        @Override
+                        public Drawable getDrawable(String source) {
+                            Drawable draw;
+                            // TODO Auto-generated method stub
+                            if (source.equals("icon_drag")) {
+                                if(position==0){
+                                    draw = getResources().getDrawable(R.drawable.seat_icon);
+                                }else if(position==1){
+                                    draw = getResources().getDrawable(R.drawable.timer_icon);
+                                }else if(position==2){
+                                    draw = getResources().getDrawable(R.drawable.suggestion_icon);
+                                }else{
+                                    draw = getResources().getDrawable(R.drawable.store_icon);
+                                }
+                                draw.setBounds(0, 0, 100, 100);
+                                return draw;
+                            }
+                            return null;
+                        }
+                    }, null);
+                    tab.setText(imageText);
+                }
             }
         }).attach();
+
     }
 
     @Override
--- app/src/main/java/com/skku/userweb/fragment/UserFragment.java
+++ app/src/main/java/com/skku/userweb/fragment/UserFragment.java
@@ -1,20 +1,40 @@
 package com.skku.userweb.fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
 import com.skku.userweb.R;
+import com.skku.userweb.activity.SelectStoreActivity;
 import com.skku.userweb.util.GlobalVar;
+import com.skku.userweb.util.Store;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,6 +42,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static java.lang.Math.sqrt;
 
 
 public class UserFragment extends Fragment {
@@ -29,11 +50,14 @@ public class UserFragment extends Fragment {
     private TextView store_name;
     private TextView totalseat;
     private TextView currentuser;
+    private String store_id;
     private String store_Name;
     private String remained_seat;
     private String img_url;
     private ImageView imgView;
     private Bitmap bitmap;
+    private Long[] arr;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,17 +70,14 @@ public class UserFragment extends Fragment {
         store_name = rootView.findViewById(R.id.fragment_user_storename);
         totalseat = rootView.findViewById(R.id.fragment_user_num_totalSeat);
         currentuser = rootView.findViewById(R.id.fragment_user_num_dailyUsers);
+        GlobalVar storeId = (GlobalVar) getActivity().getApplication();
+        store_id = storeId.getStoreId();
         GlobalVar storeName = (GlobalVar) getActivity().getApplication();
         store_Name = storeName.getStoreName();
-        GlobalVar storeSeat = (GlobalVar) getActivity().getApplication();
-        remained_seat = storeSeat.getTotalSeat();
+
         GlobalVar imgUrl = (GlobalVar) getActivity().getApplication();
         img_url = imgUrl.getImgUrl();
-
-        String seat[]=remained_seat.split("/");
         store_name.setText(store_Name);
-        totalseat.setText(seat[1]);
-        currentuser.setText(seat[0]);
         imgView = rootView.findViewById(R.id.fragment_user_profileImage);
         Thread mThread = new Thread() {
             @Override
@@ -80,9 +101,47 @@ public class UserFragment extends Fragment {
             mThread.join();
             imgView.setImageBitmap(bitmap);
 
+//        Button editbutton = (Button) rootView.findViewById(R.id.fragment_user_editButton);
+//        Button logoutbutton = (Button) rootView.findViewById(R.id.fragment_user_logoutButton);
+//
+//        editbutton.setOnClickListener(this);
+//        logoutbutton.setOnClickListener(this);
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        DocumentReference docRef = db.collection("stores").document(store_id);
+        Source source = Source.CACHE;
+        docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Document found in the offline cache
+                    DocumentSnapshot document = task.getResult();
+                    Log.d("test", "Cached document data: " + document.getData());
+                    Long num_users= (Long) document.getData().get("num_users");
+                    Long num_seats= (Long) document.getData().get("num_seats");
+                    Log.d("test", num_users+"/"+num_seats);
+                    totalseat.setText(String.valueOf(num_seats));
+                    currentuser.setText(String.valueOf(num_users));
+                } else {
+                    Log.d("test", "Cached get failed: ", task.getException());
+                }
+            }
+        });
         return rootView;
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.fragment_user_editButton:
+//                //go to edit user activity
+//            case R.id.fragment_user_logoutButton:
+//                //logout
+//
+//        }
+//    }
 }
